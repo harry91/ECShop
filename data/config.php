0309d3ae795042670453d2e992598f6f80b55f80
Gao Guangyu <330737668@qq.com>
2014-11-17 00:14:35 +0800

    revert config.php and add .gitkeep files

--- data/config.php
+++ data/config.php
@@ -3,13 +3,13 @@
 $db_host   = "localhost:3306";
 
 // database name
-$db_name   = "ecomm2";
+$db_name   = "ecomm";
 
 // database username
-$db_user   = "testecs";
+$db_user   = "root";
 
 // database password
-$db_pass   = "testecs";
+$db_pass   = "SKBskb99";
 
 // table prefix
 $prefix    = "ecs_";
@@ -32,6 +32,6 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2014-11-16 14:43:05');
+define('API_TIME', '2014-11-12 09:54:33');
 
 ?>
